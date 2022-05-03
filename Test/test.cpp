afa27dcbd7b0168e22b9bfcd7c5be8392ece0599
Viktor Kasimir <kasimir.viktor@gmail.com>
2022-05-03 19:32:59 +0200

    first commit: test stuff foir c on windows

---
+++ Test/test.cpp
@@ -0,0 +1,28 @@
+#include <windows.h>
+
+void foo(void);
+
+int CALLBACK WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdShow,
+    int nCmdShow)
+{ 
+
+    foo();
+}
+
+void foo(void)
+{
+    char test_1 = 0xF;
+    test_1 += 1;
+
+    short test_2 = 0xF;
+    test_2 += 1;
+
+    long test_3 = 0xF;
+    test_3 += 1;
+
+    long long test_4 = 0xF;
+    test_4 += 1;
+}
